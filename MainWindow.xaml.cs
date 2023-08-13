e3cd92692c197cfc33ff139dcc0e19f1cfb42341
Jiří Kruchina <kruchi@centrum.cz>
2023-08-13 16:47:37 +0200

    Add UI logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,29 +20,162 @@ namespace tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        // arrays of tile and block images in order of block IDs
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative))
+        };
+
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-Z.png", UriKind.Relative))
+        };
+
+
+        private readonly Image[,] imageControls;
+
+        // new game
+        private GameState gameState = new GameState();
+
         public MainWindow()
         {
             InitializeComponent();
+            imageControls = SetupGameCanvas(gameState.GameGrid);    // initialize game canvas
+        }
+
+        // setup image of game grid
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.Columns];    // size same as game grid
+            int cellSize = 25;  // 500 px / 20 tiles
+
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    // new cell image
+                    Image imageControl = new Image
+                    {
+                        Width = cellSize,
+                        Height = cellSize
+                    };
+
+                    // adjust distance from top left of canvas
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize);    // push top 2 hidden rows up outside canvas
+                    Canvas.SetLeft(imageControl, c * cellSize);
+
+                    // add it as UI child to canvas and save to array of all cells
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+
+            return imageControls;
+        }
+
+        // draw game grid
+        private void DrawGrid(GameGrid grid)
+        {
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    int id = grid[r, c];
+                    imageControls[r,c].Source = tileImages[id];
+                }
+            }
+        }
+
+        // draw current block
+        private void DrawBlock(Block block)
+        {
+            foreach (Position p in block.TilePositions())
+            {
+                imageControls[p.Row, p.Column].Source = tileImages[block.Id];
+            }
+        }
+
+        // draws grid and current block
+        private void Draw(GameState gameState)
+        {
+            DrawGrid(gameState.GameGrid);
+            DrawBlock(gameState.CurrentBlock);
+        }
+
+        // async = waiting without blocking UI and inputs
+        // draw UI and drop blocks
+        private async Task GameLoop()
+        {
+            Draw(gameState);
+
+            // drop by 1 every 500 ms
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(500);
+                gameState.MoveBlockDown();
+                Draw(gameState);
+            }
+
+            // when game ends -> display game over menu
+            GameOverMenu.Visibility = Visibility.Visible;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            if (gameState.GameOver) return;     // if game has ended -> ignore all keys
 
+            switch(e.Key)
+            {
+                case Key.Left:
+                    gameState.MoveBlockLeft();
+                    break;
+                case Key.Right:
+                    gameState.MoveBlockRight();
+                    break;
+                case Key.Down:
+                    gameState.MoveBlockDown();
+                    break;
+                case Key.Up:
+                    gameState.RotateBlockCW();
+                    break;
+                case Key.Z:
+                    gameState.RotateBlockCCW();
+                    break;
+                default:
+                    return;     // only redraw if player pressed a key that actually does something
+            }
+
+            Draw(gameState);
         }
 
-        private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        // when game canvas has loaded
+        private async void GameCanvas_Loaded(object sender, RoutedEventArgs e)
         {
-
+            await GameLoop();   // run game after load
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // clicked on play again button
+        private async void PlayAgain_Click(object sender, RoutedEventArgs e)
         {
+            // create new game, hide game over overlay
+            gameState = new GameState();
+            GameOverMenu.Visibility = Visibility.Hidden;
 
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
+            await GameLoop();   // run new game
         }
     }
 }
